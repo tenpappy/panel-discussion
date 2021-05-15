467bc33eec1d15e9c3ac8f71b1865dd07fda7b00
tenpappy <tenpappy@gmail.com>
2021-05-15 20:24:13 +0900

    戻し

--- src/components/atom/StatusButton.tsx
+++ src/components/atom/StatusButton.tsx
@@ -14,14 +14,14 @@ export const StatusButton: VFC<Props> = (props) => {
   const bgColorText = `text-${bgColor}-900`;
   return (
     <div>
-      <button
+      {/* <button
         className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-yellow-200 hover:bg-yellow-300 text-yellow-900  focus:outline-none`}
         onClick={() => onClick(id)}
-      >
-        {/* <button
+      > */}
+      <button
         className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 ${bgColor200} ${bgColorHover} ${bgColorText} focus:outline-none`}
         onClick={() => onClick(id)}
-      > */}
+      >
         {name}
       </button>
     </div>

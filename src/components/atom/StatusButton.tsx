87dd08e42dd15aa0c72c1acf4d9be16f8b7e2223
tenpappy <tenpappy@gmail.com>
2021-05-15 22:08:53 +0900

    tailwind.config.jsを修正しないパターン

--- src/components/atom/StatusButton.tsx
+++ src/components/atom/StatusButton.tsx
@@ -14,20 +14,30 @@ export const StatusButton: VFC<Props> = (props) => {
   const bgColorText = `text-${bgColor}-900`;
   return (
     <div>
-      {/* <button
-        className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-yellow-200 hover:bg-yellow-300 text-yellow-900  focus:outline-none`}
-        onClick={() => onClick(id)}
-      > */}
-      {/* <button
-        className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-${bgColor}-200 ${bgColorHover} ${bgColorText} focus:outline-none`}
-        onClick={() => onClick(id)}
-      > */}
-      <button
-        className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 ${bgColor200} ${bgColorHover} ${bgColorText} focus:outline-none`}
-        onClick={() => onClick(id)}
-      >
-        {name}
-      </button>
+      {bgColor === "yellow" ? (
+        <button
+          className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-yellow-200 hover:bg-yellow-300 text-yellow-900  focus:outline-none`}
+          onClick={() => onClick(id)}
+        >
+          {name}
+        </button>
+      ) : null}
+      {bgColor === "indigo" ? (
+        <button
+          className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-indigo-200 hover:bg-indigo-300 text-indigo-900  focus:outline-none`}
+          onClick={() => onClick(id)}
+        >
+          {name}
+        </button>
+      ) : null}
+      {bgColor === "gray" ? (
+        <button
+          className={`px-6 py-2 my-3 mr-2 h-10 text-base font-semibold rounded-full border-b border-purple-300 bg-gray-200 hover:bg-gray-300 text-gray-900  focus:outline-none`}
+          onClick={() => onClick(id)}
+        >
+          {name}
+        </button>
+      ) : null}
     </div>
   );
 };
--- src/components/molecule/StatusAndQuestion.tsx
+++ src/components/molecule/StatusAndQuestion.tsx
@@ -20,19 +20,26 @@ export const StatusAndQuestion: VFC<Props> = (props) => {
   const iconStyle: React.CSSProperties = { padding: 9 };
   const bgColor50 = `bg-${bgColor}-50`;
   return (
-    <div className={`${bgColor50} pb-1 rounded-3xl mx-1`}>
+    <div>
       {/* <div className={`bg-yellow-50 pb-1 rounded-3xl mx-1`}> */}
-      <div className="my-2 mx-2 flex">
-        <StatusButton onClick={onClick} id={id} bgColor={bgColor} name={name} />
-        <QuestionMessage content={question} />
-        {isDeletable && (
-          <div
-            className="p-3 cursor-pointer ml-auto"
-            onClick={() => onClickDel(id)}
-          >
-            <FontAwesomeIcon style={iconStyle} icon={faTrashAlt} />
-          </div>
-        )}
+      <div className={`${bgColor50} pb-1 rounded-3xl mx-1`}>
+        <div className="my-2 mx-2 flex">
+          <StatusButton
+            onClick={onClick}
+            id={id}
+            bgColor={bgColor}
+            name={name}
+          />
+          <QuestionMessage content={question} />
+          {isDeletable && (
+            <div
+              className="p-3 cursor-pointer ml-auto"
+              onClick={() => onClickDel(id)}
+            >
+              <FontAwesomeIcon style={iconStyle} icon={faTrashAlt} />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );

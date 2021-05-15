ea8cd23b0a749fc9be02a567f193ca268428a607
tenpappy <tenpappy@gmail.com>
2021-05-15 23:07:14 +0900

    これならいける！！かな

--- src/components/molecule/StatusAndQuestion.tsx
+++ src/components/molecule/StatusAndQuestion.tsx
@@ -20,8 +20,7 @@ export const StatusAndQuestion: VFC<Props> = (props) => {
   const iconStyle: React.CSSProperties = { padding: 9 };
   const bgColor50 = `bg-${bgColor}-50`;
   return (
-    // <div className={`${bgColor50} pb-1 rounded-3xl mx-1`}>
-    <div className={` bg-green-50 pb-1 rounded-3xl mx-1`}>
+    <div className={`${bgColor50} pb-1 rounded-3xl mx-1`}>
       <div className="my-2 mx-2 flex">
         <StatusButton onClick={onClick} id={id} bgColor={bgColor} name={name} />
         <QuestionMessage content={question} />

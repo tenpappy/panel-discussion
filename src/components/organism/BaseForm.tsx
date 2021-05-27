e6cc290f6bdb703af7daf1ae12415a4209391cfd
tenpappy <tenpappy@gmail.com>
2021-05-27 22:51:55 +0900

    input修正

--- src/components/organism/BaseForm.tsx
+++ src/components/organism/BaseForm.tsx
@@ -23,7 +23,7 @@ export const BaseForm: VFC<Props> = ({ onSubmit, buttonText, inputList }) => {
             <div key={props.type} className="mb-4">
               <input
                 {...props}
-                className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                // className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
           ))}

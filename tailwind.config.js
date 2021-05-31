feb68119d3f12a5b3fe7059d1a0201b92cdc7233
tenpappy <tenpappy@gmail.com>
2021-06-01 00:04:00 +0900

    theme.colors設定変更2

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  options: {
-    safelist: ['bg-yellow-50', 'bg-yellow-300', 'text-yellow-900', 'bg-indigo-50', 'bg-indigo-300', 'text-indigo-900', 'bg-gray-50', 'bg-gray-300', 'text-gray-900'],
-  },
+  // options: {
+  //   safelist: ['bg-yellow-50', 'bg-yellow-300', 'text-yellow-900', 'bg-indigo-50', 'bg-indigo-300', 'text-indigo-900', 'bg-gray-50', 'bg-gray-300', 'text-gray-900'],
+  // },
 }

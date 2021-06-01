62972d2d4ea5d98c38ad8a88002587e08a90f6be
tenpappy <tenpappy@gmail.com>
2021-06-01 23:48:52 +0900

    configに設定削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  future: { webpack5: false },
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  // future: { webpack5: false },
   theme: {
-    colors: {
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-      gray: colors.gray,
-      white: colors.white,
-      green: colors.green,
-      purple: colors.purple,
-    },
+    // colors: {
+    //   indigo: colors.indigo,
+    //   yellow: colors.yellow,
+    //   gray: colors.gray,
+    //   white: colors.white,
+    //   green: colors.green,
+    //   purple: colors.purple,
+    // },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  options: {
-    safelist: ['bg-yellow-50', 'bg-yellow-300', 'text-yellow-900', 'bg-indigo-50', 'bg-indigo-300', 'text-indigo-900', 'bg-gray-50', 'bg-gray-300', 'text-gray-900'],
-  },
+  // options: {
+  //   safelist: ['bg-yellow-50', 'bg-yellow-300', 'text-yellow-900', 'bg-indigo-50', 'bg-indigo-300', 'text-indigo-900', 'bg-gray-50', 'bg-gray-300', 'text-gray-900'],
+  // },
 }

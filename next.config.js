0e93a6c337eebadc0b2f574580f450582a089782
tenpappy <tenpappy@gmail.com>
2021-06-10 22:31:19 +0900

    config修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    SUPABASE_URL: "https://ahnxzdlskibosjdmbohw.supabase.co",
-    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDE4Mzg4OCwiZXhwIjoxOTM1NzU5ODg4fQ.e1zVwbNSUc7MA4M_dfhly0OXovuVmwpf4fZySpJP4WQ",
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY
   },
 };
